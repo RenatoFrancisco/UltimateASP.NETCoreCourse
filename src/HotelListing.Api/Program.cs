8028a8d6689f7c63facef9f0baedd9dcc097192e
renatofrancisco <renatofrancisco.s@gmail.com>
2024-06-01 18:57:47 -0300

    Configured SeriLog and Seq

--- src/HotelListing.Api/Program.cs
+++ src/HotelListing.Api/Program.cs
@@ -1,15 +1,24 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        b => b.AllowAnyHeader()
+               .AllowAnyOrigin()
+               .AllowAnyMethod());
+});
+
+builder.Host.UseSerilog((ctx, lc) => 
+    lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
