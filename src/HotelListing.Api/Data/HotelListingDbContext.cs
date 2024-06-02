e490295ba57a136b9731a0095d6f380b6f5570e6
renatofrancisco <renatofrancisco.s@gmail.com>
2024-06-01 23:35:35 -0300

    Implement Data Class and Perform Migrations

--- src/HotelListing.Api/Data/HotelListingDbContext.cs
+++ src/HotelListing.Api/Data/HotelListingDbContext.cs
@@ -4,4 +4,6 @@ namespace HotelListing.Api.Data;
 
 public class HotelListingDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Country> Countries { get; set; }
 }
