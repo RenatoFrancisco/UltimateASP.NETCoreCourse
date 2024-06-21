792cf1052692db2cef1871cad0a3374a07a01c9b
renatofrancisco <renatofrancisco.s@gmail.com>
2024-06-20 21:41:25 -0300

    Add Logging

--- src/HotelListing.Api/Controllers/AccountController.cs
+++ src/HotelListing.Api/Controllers/AccountController.cs
@@ -6,7 +6,7 @@ namespace HotelListing.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AccountController(IAuthManager authManager): ControllerBase
+public class AccountController(IAuthManager authManager, ILogger<AccountController> logger): ControllerBase
 {
     [HttpPost("register")]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -14,14 +14,20 @@ public class AccountController(IAuthManager authManager): ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult> Register(ApiUserDto apiUserDto)
     {
+        logger.LogInformation($"Registration attempt for user with email {apiUserDto.Email}.");
+
         var errors = await authManager.Register(apiUserDto);
         if (errors.Any())
-        { 
+        {
+            logger.LogWarning($"Error during Registration attempt for user with email {apiUserDto.Email}.");
+
             foreach (var error in errors)
                 ModelState.AddModelError(error.Code, error.Description);
 
             return BadRequest(ModelState);
         }
+
+        logger.LogInformation($"Registration for user with email {apiUserDto.Email} has been successfully completed.");
 
         return Ok();
     }
@@ -32,9 +38,17 @@ public class AccountController(IAuthManager authManager): ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult> Login(LoginDto  loginDto)
     {
+        logger.LogInformation($"Login attempt for user with email {loginDto.Email}.");
+
         var authResponse = await authManager.Login(loginDto);
 
-        if (authResponse is null) return Unauthorized();
+        if (authResponse is null)
+        {
+            logger.LogWarning($"User with email {loginDto.Email} not authorized.");
+            return Unauthorized();
+        }
+
+        logger.LogInformation($"Login for user with email {loginDto.Email} has been successfully authorized.");
 
         return Ok(authResponse);
     }
@@ -45,9 +59,17 @@ public class AccountController(IAuthManager authManager): ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult> RefreshToken(AuthResponseDto request)
     {
+        logger.LogInformation($"Refresh token attempt for user with id {request.UserId}.");
+
         var authResponse = await authManager.VerifyRefreshToken(request);
 
-        if (authResponse is null) return Unauthorized();
+        if (authResponse is null)
+        {
+            logger.LogWarning($"User with id {request.UserId} not authorized.");
+            return Unauthorized();
+        }
+
+        logger.LogInformation($"Refresh token for user with id {request.UserId} has been successfully generated.");
 
         return Ok(authResponse);
     }
