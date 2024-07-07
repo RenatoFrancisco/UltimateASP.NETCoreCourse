1fa62865ba4e4fc56d81b0643a708b38c7f532ba
renatofrancisco <renatofrancisco.s@gmail.com>
2024-07-07 11:29:30 -0300

    Repository Refactor

--- src/HotelListing.Api.Core/Contracts/IGenericRepository.cs
+++ src/HotelListing.Api.Core/Contracts/IGenericRepository.cs
@@ -1,15 +1,19 @@
-﻿using HotelListing.Api.Data;
-using HotelListing.Api.Core.Models;
+﻿using HotelListing.Api.Core.Models;
+using HotelListing.Api.Data;
 
 namespace HotelListing.Api.Core.Contracts;
 
 public interface IGenericRepository<T> where T : Entity, new()
 {
-    Task<T> GetAsync(int id);
     Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<TResult>> GetAllAsync<TResult>();
     Task<PagedResult<TResult>> GetAllAsync<TResult>(QueryParameters queryParameters);
-    Task<T> Addsync(T entity);
-    Task UpdateAsync(T entity);
-    Task DeleteAsync(int id);
+    Task<T> GetAsync(int? id);
+    Task<TResult> GetAsync<TResult>(int? id);
     Task<bool> Exists(int id);
+    Task<T> Addsync(T entity);
+    Task<TResult> Addsync<TSource, TResult>(TSource source);
+    Task UpdateAsync(T entity);
+    Task UpdateAsync<TSource>(int? id, TSource source);
+    Task DeleteAsync(int id);
 }
--- src/HotelListing.Api.Core/Repositories/GenericRepository.cs
+++ src/HotelListing.Api.Core/Repositories/GenericRepository.cs
@@ -1,8 +1,9 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using HotelListing.Api.Core.Contracts;
-using HotelListing.Api.Data;
+using HotelListing.Api.Core.Exceptions;
 using HotelListing.Api.Core.Models;
+using HotelListing.Api.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.Api.Core.Repositories;
@@ -11,8 +12,12 @@ public class GenericRepository<T>(HotelListingDbContext context, IMapper mapper)
 {
     protected DbSet<T> DbSet { get; } = context.Set<T>();
 
-
     public async Task<IEnumerable<T>> GetAllAsync() => await DbSet.ToListAsync();
+
+    public async Task<IEnumerable<TResult>> GetAllAsync<TResult>() =>
+        await context.Set<T>()
+            .ProjectTo<TResult>(mapper.ConfigurationProvider)
+            .ToListAsync();
 
     public async Task<PagedResult<TResult>> GetAllAsync<TResult>(QueryParameters queryParameters)
     {
@@ -32,7 +37,26 @@ public class GenericRepository<T>(HotelListingDbContext context, IMapper mapper)
         };
     }
 
-    public async Task<T> GetAsync(int id) => await DbSet.FindAsync(id);
+    public async Task<T> GetAsync(int? id) => await DbSet.FindAsync(id);
+
+    public async Task<TResult> GetAsync<TResult>(int? id)
+    {
+        if (id is null)
+            throw new NotFoundException(typeof(T).Name, id.HasValue ? id : "No key provided");
+
+        var result = await DbSet.FindAsync(id);
+        return mapper.Map<TResult>(result);
+
+    }
+
+    public async Task<TResult> Addsync<TSource, TResult>(TSource source)
+    {
+        var entity = mapper.Map<T>(source);
+        await context.AddAsync(entity);
+        await context.SaveChangesAsync();
+
+        return mapper.Map<TResult>(entity);
+    }
 
     public async Task<bool> Exists(int id) => await DbSet.AnyAsync(x => x.Id == id);
 
@@ -45,6 +69,17 @@ public class GenericRepository<T>(HotelListingDbContext context, IMapper mapper)
     public async Task UpdateAsync(T entity)
     {
         DbSet.Update(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync<TSource>(int? id, TSource source)
+    {
+        var entity = await GetAsync(id);
+        if (entity is null)
+            throw new NotFoundException(typeof(T).Name, id.HasValue ? id : "No key provided");
+
+        mapper.Map(source, entity);
+        context.Update(entity);
         await context.SaveChangesAsync();
     }
 
